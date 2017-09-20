020b6af5e9fb25a9ac10241b75901ad59eb6277d
derekbez <derekbez@hotmail.com>
2017-09-20 13:49:46 +0100

    Add project files.

---
+++ MapleLodgeSpeciesDatabase/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace MapleLodgeSpeciesDatabase.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ MapleLodgeSpeciesDatabase/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MapleLodgeSpeciesDatabase.Global" Language="C#" %>
